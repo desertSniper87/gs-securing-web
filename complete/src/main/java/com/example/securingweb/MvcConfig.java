bac98fbbf66048d01795f7b8498f3ac07588f02a
desertsniper87 <torshobuet@gmail.com>
2023-01-16 18:35:25 +0600

    interceptor added

--- complete/src/main/java/com/example/securingweb/MvcConfig.java
+++ complete/src/main/java/com/example/securingweb/MvcConfig.java
@@ -1,5 +1,7 @@
 package com.example.securingweb;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -21,6 +23,15 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new HandlerInterceptor() {
+			public boolean preHandle(HttpServletRequest request,
+									 HttpServletResponse response, Object handler)
+					throws Exception {
+
+				long startTime = System.currentTimeMillis();
+				request.setAttribute("startTime", startTime);
+
+				return true;
+			}
 		});
 	}
 
