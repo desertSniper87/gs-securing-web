82568abe0a07e044e994eda731829166579538a1
desertsniper87 <torshobuet@gmail.com>
2023-01-16 18:05:31 +0600

    update from e203mah

--- complete/src/main/java/com/example/securingweb/MvcConfig.java
+++ complete/src/main/java/com/example/securingweb/MvcConfig.java
@@ -1,6 +1,9 @@
 package com.example.securingweb;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +16,12 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new HandlerInterceptor() {
+		});
 	}
 
 }
